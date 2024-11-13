5f661e0c8fd02f6a02b65a355297eff42b729747
Wanderson Lira <wandersonlira.dev1@gmail.com>
2024-11-13 14:21:46 -0300

    :broom: cleanup: Eliminando blocos de código comentados

--- src/main/java/com/lira/security/project/model/entities/UserEntity.java
+++ src/main/java/com/lira/security/project/model/entities/UserEntity.java
@@ -8,15 +8,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity(name = "Usuario")
 
 public class UserEntity implements Serializable{
